830ae141471fdc81eb98be3e0f00deef0643e0eb
Jonathan Henrique <silva.henriquejonas@gmail.com>
2022-12-11 11:47:24 -0300

    add spring.jpa.defer-datasource-initialization=true e nome script import.sql - lendo

--- src/main/java/com/etec/tcc/sprint_quiz/SprintQuizApplication.java
+++ src/main/java/com/etec/tcc/sprint_quiz/SprintQuizApplication.java
@@ -7,21 +7,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class SprintQuizApplication {
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(@Autowired ProvaRepository provaRepository) {
-//		return args -> {
-//			JwtService jwt = new JwtService();
-//			Usuario usuario = Usuario.builder().email("fulano@gmail.com").build();
-//			String token = jwt.gerarToken(usuario);
-//			System.out.println(token);
-//			
-//			boolean isTokenValido = jwt.tokenValido(token);
-//			System.out.println("O token está válido?: " + isTokenValido);
-//			
-//			System.out.println(jwt.obterLoginUsuario(token));
-//		};
-//
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprintQuizApplication.class, args);
